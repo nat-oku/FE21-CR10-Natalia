7d0d26ba21a034ee0b357e88b7b052f465c28708
Natalia <natalia_bochenek@hotmail.com>
2021-04-23 12:36:20 +0200

    small changes to structure

--- components/navbar.php
+++ components/navbar.php
@@ -1,8 +1,8 @@
 <!-- add layout for the navbar visible on all sites -->
 
-<div class="bg-image d-flex justify-content-center align-items-center">
-  <nav class="align-self-start mt-5">
-    <ul class="nav justify-content-center">
+<div class="bg-image d-flex ">
+  <nav class="align-self-start pt-5">
+    <ul class="nav justify-content-center flex-column m-2">
       <li class="nav-item">
         <a class="nav-link active fs-5" href="#">Home</a>
       </li>
@@ -18,7 +18,7 @@
     <!-- fullpage background pic -->
   <div class="mask maskBg">  <!-- the mask will help us make the img darker so our content will be more visible -->
     <div class="d-flex justify-content-center align-items-start">
-      <h1 class="textColor-1 font-1 text-center">Welcome N@t's Lib</h1>
+      <h1 class="textColor-1 font-1 text-center p-5">Welcome to N@t's Lib</h1>
     </div>
   </div>
 </div>
