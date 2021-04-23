a1bd2d5203b751d2bdebf7defcd4ccf040ec6202
Natalia <natalia_bochenek@hotmail.com>
2021-04-23 11:10:25 +0200

    adding basic layout to navbar

---
+++ components/navbar.php
@@ -0,0 +1,12 @@
+<ul class="nav justify-content-center">
+  <a class="navbar-brand" href="#">N@at's Lib</a>
+  <li class="nav-item">
+    <a class="nav-link active" href="#">Home</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="#">Items</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="#">Publishers</a>
+  </li>
+</ul>
