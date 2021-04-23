825c98d90c4d1ce88b726acf509943b1ff0da5a6
Natalia <natalia_bochenek@hotmail.com>
2021-04-23 11:36:41 +0200

    deleting awesome icons

--- components/boot_CSS.php
+++ components/boot_CSS.php
@@ -3,6 +3,3 @@
 
 <!-- MDB Bootstrap-->
 <link href="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/3.3.0/mdb.min.css" rel="stylesheet" />
-
-<!-- awesome icons --->
-<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.1/css/all.css" integrity="sha384-vp86vTRFVJgpjF9jiIGPEEqYqlDwgyBgEF109VFjmqGmIY/Y4HV4d3Gp2irVfcrp" crossorigin="anonymous">
