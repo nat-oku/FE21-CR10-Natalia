b44c6e150d0b03e1ddf6075e384c20912824a0b9
Natalia <natalia_bochenek@hotmail.com>
2021-04-23 12:59:40 +0200

    updating $tbody variable

--- actions/table_connection.php
+++ actions/table_connection.php
@@ -6,14 +6,22 @@
   $tbody='';
   if(mysqli_num_rows($result)  > 0) {    
       while($row = mysqli_fetch_array($result, MYSQLI_ASSOC)){    
-        $tbody .= "<tr>
+        $tbody .= "
+        <tr>
 
-            <td><img class='img-thumbnail' src='pictures/" .$row['img']."'</td>
+            <td><img class='img-thumbnail' src='img/" .$row['img']."'</td>
             <td>" .$row['mediaType']."</td>
-              <td>" .$row['title']."</td>
+            <td>" .$row['title']."</td>
+            <td>" .$row['authorFirstName']." ".$row['authorLastName']."</td>
+            <td>" .$row['shortDesrc']."</td>
+            <td>" .$row['ISBN']."</td>
+            <td>" .$row['publishDate']."</td>
+            <td>" .$row['publisherName']."</td>
+            <td>" .$row['size']."</td>
+            <td>" .$row['status']."</td>
             <td><a href='update.php?id=" .$row['itemID']."'><button class='btn btn-primary btn-sm' type='button'>Edit</button></a>
             <a href='delete.php?id=" .$row['itemID']."'><button class='btn btn-danger btn-sm' type='button'>Delete</button></a></td>
-              </tr>";
+        </tr>";
     };
   } else {
     $tbody =  "<tr><td colspan='5'><center>No Data Available </center></td></tr>";
