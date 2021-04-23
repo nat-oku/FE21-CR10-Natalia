972d062b0913865faebee34ffbddbf3543768fea
Natalia <natalia_bochenek@hotmail.com>
2021-04-23 11:15:05 +0200

    adding basic structure to footer&navbar

---
+++ components/footer.php
@@ -0,0 +1,20 @@
+<!-- add layout for the navbar visible on all sites -->
+<nav class="navbar navbar-light bg-light">
+  <div class="d-flex flex-column align-items-center w-100">
+      <a class="navbar-brand fs-1 text-gold fw-light" routerLink="/"><i class="text-gold fas fa-route"></i><span class="text-gold fw-light"> N@t's Lib</span></a> 
+      <ul class="nav text-uppercase flex-row">
+      <li class="nav-item">
+        <a class="nav-link fs-4" href="#"><span class="text-gold fw-light">Home</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link fs-4" href="#"><span class="text-gold fw-light">Items</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link fs-4" href="#"><span class="text-gold fw-light">Publishers</span></a>
+      </li>
+    </ul>
+    <div class="w-100 footerCopyrights d-flex justify-content-center align-items-end">
+      <p class="fs-6 pt-3 text-gold fw-light">&#169; Natalia Bochenek CodeFactory 2021</p>
+    </div>
+  </div>
+</nav>
