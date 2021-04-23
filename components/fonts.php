0eb14e4f866c16fa3df6f780ae87b3f1acaa7a53
Natalia <natalia_bochenek@hotmail.com>
2021-04-23 12:16:03 +0200

    adding fonts

---
+++ components/fonts.php
@@ -0,0 +1,6 @@
+<!-- awesome icons --->
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.1/css/all.css" integrity="sha384-vp86vTRFVJgpjF9jiIGPEEqYqlDwgyBgEF109VFjmqGmIY/Y4HV4d3Gp2irVfcrp" crossorigin="anonymous">
+
+<!-- Google Fonts -->
+<link rel="preconnect" href="https://fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css2?family=Raleway:wght@100;300;400&display=swap" rel="stylesheet">
