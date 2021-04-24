f9711d73016b822e5801ec667c0eb310c13c2500
Natalia <natalia_bochenek@hotmail.com>
2021-04-24 11:25:23 +0200

    updating layout delete.php

--- delete.php
+++ delete.php
@@ -34,7 +34,6 @@ if  ($_GET['id']) {
    <head>
      <meta  charset="UTF-8">
      <meta name="viewport"  content="width=device-width, initial-scale=1.0">
-     <title>Delete Product</title>
      
      <!-- inserting fonts php file -->
      <?php require_once 'components/fonts.php'?>
@@ -42,28 +41,34 @@ if  ($_GET['id']) {
      <!-- inserting Bootstrap -->
      <?php require_once 'components/boot_CSS.php'?>
      <!-- CSS only for this project -->
-     <link rel="stylesheet" href="css/style.css">       
+     <link rel="stylesheet" href="css/style.css"> 
+     
+     <title>N@t's Lib || Delete Product</title>
+
    </head>
    <body>
-     <div class="container pt-3 pb-3">
-     <h2 class='textColor-4 text-center'>Delete request</h2>
-     <fieldset>
-           <legend class='h2 mb-3'>  <img class='img-thumbnail'  src='img/<?php echo $img ?>' alt="<?php echo $img ?>"></legend >
-           <h5>You have selected the data below: </h5>
+     <div class="container">
+       <h2 class='textColor-4 text-center mt-3 mb-3'>Delete request</h2>
+       <div class="d-lg-flex justify-content-around">
+         <img class='img-thumbnail'  src='img/<?php echo $img ?>' alt="<?php echo $img ?>">
+         
+         <fieldset>
+           <h5 >You have selected the item below: </h5>
            <table class="table w-75 mt-3" >
-               <tr>
-                    <td><?php echo "$title by $authorFirstName $authorLastName" ?></td>
-                </tr>
+             <tr>
+               <td><?php echo "$title by $authorFirstName $authorLastName" ?></td>
            </table>
-
-           <h3  class="mb-4">Do you really want to delete this product?</h3>
+           <h3  class="mb-4">Do you really want to delete this item?</h3>
            <form action ="actions/a_delete.php"  method="post">
-               <input type="hidden"  name="id" value ="<?php echo $id ?>" />
-               <input type="hidden"  name="img"  value="<?php echo $img ?>" />
-               <button class="btn btn-danger"  type="submit"> Yes, delete it! </button>
-                <a href="index.php"><button class="btn btn-warning"  type="button"> No, go back! </button></a>
+            <input type="hidden"  name="id" value ="<?php echo $id ?>" />
+            <input type="hidden"  name="img"  value="<?php echo $img ?>" />
+            <button class="btn bgColor-5 textColor-1"  type="submit"> Yes, delete it! </button>
+            <a href="index.php"><button class="btn bgColor-4 textColor-1"  type="button">No, go back!</button></a>
            </form>
        </fieldset>
+     </div>
+
+     
      </div>
 
       
